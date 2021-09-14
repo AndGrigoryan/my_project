6f590a48c6ac73079b1b472ee4a98f463650f017
Andranik Grigoryan <andranik.grigoryan.02@gmail.com>
2021-09-14 16:58:40 +0400

    Ex2_60

--- main.cpp
+++ main.cpp
@@ -1,33 +1,25 @@
 #include <iostream>
 
+void NOD(int a, int b) {
+    if (a > b) {
+        a -= b;
+    }
+    else {
+        b -= a;
+    }
+    if(b == 0) {
+        std::cout << a;
+        return;
+    }
+
+    NOD(a, b);
+}
+
 int main() {
 
-    int num1;
-    bool b = true;
-    std::cin >> num1;
-
-    for(int i = 1; i <= 9; i++) {
-
-        int copy_num1 = num1 * 10 + i;
-        int temp = copy_num1;
-        int num2 = 0;
-
-        while(temp != 0) {
-            num2 = num2 * 10 + temp % 10;
-            temp /= 10;
-        }
-
-        if(copy_num1 == num2) {
-            std::cout << "Yes!" << " i = " << i << std::endl;
-            b = false;
-            break;
-        }
-
-    }
-
-    if( b == true) {
-        std::cout << "No!" << std::endl;
-    }
+    int num1, num2;
+    std::cin >> num1 >> num2;
+    NOD(num1, num2);
 
     return 0;
 }
