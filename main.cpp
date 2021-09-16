28d795913e99cd04b9d671610a5227cd7ead222b
Andranik Grigoryan <andranik.grigoryan.02@gmail.com>
2021-09-16 19:10:40 +0400

    Calculator

--- main.cpp
+++ main.cpp
@@ -2,15 +2,37 @@
 
 int main() {
 
-    int a, b, c;
+    int num1, num2;
+    char operation;
+    bool power = true;
 
-    std::cin >> a >> b >> c;
+    while( power  ) {
+        std::cin >> num1 >> operation >> num2;
 
-    if( (a + b > c) || (a + c > b) || (b + c > a) ) {
-        std::cout << "Yes!" << std::endl;
-    }
-    else {
-        std::cout << "No!" << std::endl;
+        switch (operation) {
+            case '+':
+                std::cout << num1 << " + " << num2 << " = " << num1 + num2 << std::endl;
+                break;
+            case '-':
+                std::cout << num1 << " - " << num2 << " = " << num1 - num2 << std::endl;
+                break;
+            case '*':
+                std::cout << num1 << " * " << num2 << " = " << num1 * num2 << std::endl;
+                break;
+            case '/':
+                if(num2 == 0) {
+                    std::cout << "Null\n";
+                    break;
+                }
+                std::cout << num1 << " / " << num2 << " = " << num1 / num2 << std::endl;
+                break;
+            default :
+                std::cout << "Invalid symbol";
+                break;
+        }
+
+        std::cout << "If you want to count a number again, write 1, otherwise write 0:";
+        std::cin >> power;
     }
 
     return 0;
