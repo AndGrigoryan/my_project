aaa291c3651d56c8ba3331356b31ac7a262e4f26
Andranik Grigoryan <andranik.grigoryan.02@gmail.com>
2021-09-17 23:36:58 +0400

    Selective sorting

--- main.cpp
+++ main.cpp
@@ -5,20 +5,29 @@ int main()
     const int arr_length = 4;
     int arr[arr_length] = {1, 4, 2, 6};
 
-    for (int i = 0; i < arr_length - 1; ++i) {
-        for (int y = i + 1; y < arr_length; ++y) {
-            if(arr[i] > arr[y]) {
-               int temp = arr[i];
-               arr[i] = arr[y];
-               arr[y] = temp;
-            }
-        }
-    }
+   int start = 0, min, min_pos;
+
+   while (start != arr_length - 1) {
+       min = arr[start];
+       min_pos = start;
+
+       for (int i = start; i < arr_length; ++i) {
+           if (arr[i] < min) {
+               min = arr[i];
+               min_pos = i;
+           }
+       }
+       if(min_pos != start) {
+           int temp = arr[start];
+           arr[start] = arr[min_pos];
+           arr[min_pos] = temp;
+       }
+       start++;
+   }
 
     for (int i = 0; i < arr_length; ++i) {
         std::cout << arr[i] << ' ';
     }
-
 
     return 0;
 }
