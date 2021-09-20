ea0642c07ee9e4ad892e9e07794d2ada7f6064f7
Andranik Grigoryan <andranik.grigoryan.02@gmail.com>
2021-09-20 20:00:13 +0400

    quicksort

--- main.cpp
+++ main.cpp
@@ -1,72 +1,50 @@
-#include "iostream"
+#include <iostream>
 
-const int length = 5;
+int arr[9] = {5, 2, 7, 2, 13, 3, 8, 15, 19};
 
-struct Dog{
-    char eye_color[6];
-    int height;
-    int age = 2;
-};
+void quicksort(int high, int low) {
+    int j = high;
+    int i = low;
+    int p = arr[(high + low) / 2];
 
-struct Computer {
-    char system[15] = "Windows 10";
-    int cpu_core_count;
-    int ram;
-};
+    do {
+        while (arr[i] < p) {
+            i++;
+        }
+        while(arr[j] > p) {
+            j--;
+        }
 
-struct User {
-    int age ;
-    char name[20] = "User";
-    Dog lynda;
-    Computer laptop;
+        if(i <= j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }while(i <= j);
 
-};
+    if(j > low) {
+        quicksort(j, low);
+    }
+    if (high > i) {
+        quicksort(high, i);
+    }
 
-struct House {
 
-    int length = 10;
-    int width;
-    User u[];
 
-};
 
-void print_data(User u) {
-
-    std::cout << std::endl << "User age: " << u.age;
-    std::cout << std::endl << "User name: " << u.name;
-
-    std::cout << std::endl << "User dog height: " << u.lynda.height;
-    std::cout << std::endl << "User dog eye color: " << u.lynda.eye_color;
-
-    std::cout << std::endl << "User laptop system: " << u.laptop.system;
-    std::cout << std::endl << "User RAM: " << u.laptop.ram;
-    std::cout << std::endl << "User laptop cpu: " << u.laptop.cpu_core_count;
 }
 
 int main() {
 
-    User test;
+    const int arr_length = 9;
 
-    test.age = 21;
+    quicksort(arr_length - 1, 0);
 
-    test.lynda.height = 2;
-
-    std::cin >> test.lynda.eye_color;
-
-    test.laptop.cpu_core_count = 9;
-    test.laptop.ram = 16;
-
-    House user1_tun;
-
-
-
-    print_data(test);
-
-    user1_tun.u[0].laptop.ram = 21;
-    user1_tun.u[1].laptop.ram = 42;
-
-    std::cout << std::endl << "User1 laptop ram: " << user1_tun.u[0].laptop.ram;
-    std::cout << std::endl << "User1 laptop ram: " << user1_tun.u[1].laptop.ram;
+    for (int i = 0; i < arr_length; ++i) {
+        std::cout << arr[i] << " ";
+    }
 
     return 0;
 }
