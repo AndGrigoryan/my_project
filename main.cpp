1462a26ef6bb86aca19b73a2d6af81051852d0e2
Andranik Grigoryan <andranik.grigoryan.02@gmail.com>
2021-10-16 19:36:27 +0400

    erankyun rec-ov

--- main.cpp
+++ main.cpp
@@ -1,17 +1,22 @@
 #include <iostream>
 
-int main() {
-
-    int first_row = 4;
-    std::string g = "";
-    for (int i = first_row; i > 0; i--) {
+void rec(int f_row, std::string g) {
+    if(f_row > 0) {
         std::cout << g;
-        for (int j = 0; j < i; j++) {
+        for (int j = 0; j < f_row; j++) {
             std::cout << "* ";
         }
         std::cout << std::endl;
         g += " ";
+        rec(f_row - 1, g);
     }
+}
 
+int main() {
+
+    int first_row = 4;
+    std::string g = "";
+
+    rec(first_row, g);
     return 0;
 }
