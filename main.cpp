d4b89abd288d7c75ff110352d4e3b8f0d5e34f2a
Andranik Grigoryan <andranik.grigoryan.02@gmail.com>
2021-10-16 19:43:02 +0400

    triangle

--- main.cpp
+++ main.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 
-void rec(int f_row, std::string g) {
+void rec(int f_row, std::string num_sp) {
     if(f_row > 0) {
-        std::cout << g;
+        std::cout << num_sp;
         for (int j = 0; j < f_row; j++) {
             std::cout << "* ";
         }
         std::cout << std::endl;
-        g += " ";
-        rec(f_row - 1, g);
+        num_sp += " ";
+        rec(f_row - 1, num_sp);
     }
 }
 
 int main() {
 
     int first_row = 4;
-    std::string g = "";
+    std::string number_spaces = "";
 
-    rec(first_row, g);
+    rec(first_row, number_spaces);
     return 0;
 }
