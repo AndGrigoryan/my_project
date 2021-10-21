d8685a778fbef74ed6b4c8acb18dbf5066275db8
Andranik Grigoryan <andranik.grigoryan.02@gmail.com>
2021-10-21 14:24:30 +0400

    Working with string

--- main.cpp
+++ main.cpp
@@ -1,48 +1,19 @@
 #include <iostream>
-
-class Number {
-        private:
-            int m_number;
-        public:
-    
-            Number(int number=0)
-            : m_number(number) {
-
-            }
-
-            Number& operator++();
-            Number& operator--();
-
-            friend std::ostream& operator<< (std::ostream &out, const Number &n);
-        };
-
-Number& Number::operator++() {
-
-   --m_number;
-
-    return *this;
-}
-
-Number& Number::operator--() {
-    ++m_number;
-
-    return *this;
-}
-
-std::ostream& operator<< (std::ostream &out, const Number &n) {
-    out << n.m_number;
-    return out;
-}
+#include "string"
 
 int main() {
 
-    Number number(7);
+    std::string x;
 
-    std::cout <<   number << std::endl;
-    std::cout << ++number << std::endl;
-    std::cout << ++number << std::endl;
-    std::cout << --number << std::endl;
-    std::cout << --number << std::endl;
+    std::getline(std::cin, x);
+
+    int i = x.find("dummy");
+
+    if (std::string::npos != i ) {
+        x.replace(i - 1 + 5, 1, "i ");
+    }
+
+    std::cout << x << std::endl;
 
     return 0;
 }
