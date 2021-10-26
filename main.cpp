644cd8f5f7a976bf22d12b4473bac34d9b879cfb
Andranik Grigoryan <andranik.grigoryan.02@gmail.com>
2021-10-26 16:23:09 +0400

    sandwich string

--- main.cpp
+++ main.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
 #include "string"
 
+std::string solver (std::string text) {
+    if (text.size() <= 2) {
+        return "No";
+    }
+    int cnt = 0;
+    char t = 0;
+    for (int i = 0; i <= text.size() / 2; i++) {
+        if ( text[i] != text[text.size() - i - 1]) {
+            return "No";
+        }
+        if (text[i] != t) {
+            cnt++;
+            t = text[i];
+        }
+        if ( cnt > 2) {
+            return "No";
+        }
+    }
+    if (cnt <= 1) {
+        return "No!";
+    }
+    return "Yes";
+}
+
 int main() {
 
-    std::string x;
+    std::string text;
 
-    std::getline(std::cin, x);
+    std::cin >> text;
 
-    int i = x.find("dummy");
-
-    if (std::string::npos != i ) {
-        x.replace(i - 1 + 5, 1, "i ");
-    }
-
-    std::cout << x << std::endl;
+    std::cout << solver(text) << std::endl;
 
     return 0;
 }
