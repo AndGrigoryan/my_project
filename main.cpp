ee44aaf057dee14f7393f77d83afe55f38f25221
Andranik Grigoryan <andranik.grigoryan.02@gmail.com>
2021-10-31 21:23:54 +0400

    String

--- main.cpp
+++ main.cpp
@@ -1,66 +1,15 @@
 #include <iostream>
-
-class Animal {
-    int age;
-    std::string name;
-
-public:
-
-    static void print_hello() {
-        std::cout << "Hello" << "\n";
-    }
-
-    Animal(int a, std::string n) {
-        this -> name = n;
-        this -> age = a;
-    }
-    ~Animal(){                                                                              //destructor
-        std::cout << "Name: " << this -> name << "\n" << "Age: " << this -> age << "\n";
-    }
-};
-
-class User : public Animal{
-    std::string surname;
-
-protected:
-    void esim() {
-        std::cout << "Protected method" << "\n";
-    }
-
-public:
-    User(std::string s, std::string n, int a) : Animal(a, n) {
-        this -> surname = s;
-    }
-    ~User() {
-        std::cout << "Surname: " << this -> surname << "\n";
-        esim();
-    }
-};
-
-struct Human {
-    std::string name;
-    int age;
-    int height;
-};
-
-void show_struct(Human* u) {
-    std::cout << "Name: " << u -> name << std::endl;
-    std::cout << "Age: " << u -> age << std::endl;
-    std::cout << "Height: " << u -> height << std::endl;
-}
+#include "string"
 
 int main() {
 
-    User carl("Johnson", "Bac", 12);
+    std::string text = "Python is an interpreted high-level general-purpose programming language. Its design philosophy emphasizes code readability with its use of significant indentation. Its language constructs as well as its object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects";
 
-    Animal::print_hello();
+    std::string test = "design";
+    int i = text.find(test);
 
-//    Human user;
-//    user.name = "John";
-//    user.height = 188;
-//    user.age = 21;
-//
-//    show_struct(&user);
+    std::cout << i << "  <<------>>  " << i - 1 + test.length();
+
 
     return 0;
 }
