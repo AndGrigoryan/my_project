57c922fe9a04af631af8e4d26c6d584ad9e40ee7
Andranik Grigoryan <andranik.grigoryan.02@gmail.com>
2021-10-31 20:31:40 +0400

    Struct

--- main.cpp
+++ main.cpp
@@ -1,29 +1,50 @@
 #include <iostream>
 
-class Animal {
-    int age;
+//
+//class Animal {
+//    int age;
+//    std::string name;
+//
+//public:
+//
+//    static void print_hello() {
+//        std::cout << "Hello" << "\n";
+//    }
+//
+//    Animal(int a, std::string n) {
+//        this -> name = n;
+//        this -> age = a;
+//    }
+//    ~Animal(){
+//        std::cout << "Name: " << this -> name << "\n" << "Age: " << this -> age << "\n";
+//    }
+//};
+
+struct Human {
     std::string name;
-
-public:
-
-    static void print_hello() {
-        std::cout << "Hello" << "\n";
-    }
-
-    Animal(int a, std::string n) {
-        this -> name = n;
-        this -> age = a;
-    }
-    ~Animal(){
-        std::cout << "Name: " << this -> name << "\n" << "Age: " << this -> age << "\n";
-    }
+    int age;
+    int height;
 };
+
+void show_struct(Human* u) {
+    std::cout << "Name: " << u -> name << std::endl;
+    std::cout << "Age: " << u -> age << std::endl;
+    std::cout << "Height: " << u -> height << std::endl;
+}
 
 int main() {
 
-    Animal dog(12, "Bac");
+//    Animal dog(12, "Bac");
+//
+//    Animal::print_hello();
 
-    Animal::print_hello();
+    Human user;
+
+    user.name = "John";
+    user.height = 188;
+    user.age = 21;
+
+    show_struct(&user);
 
     return 0;
 }
