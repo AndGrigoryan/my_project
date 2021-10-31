12962335ef3313c64d817b9f981258042cd5eb82
Andranik Grigoryan <andranik.grigoryan.02@gmail.com>
2021-10-31 20:09:51 +0400

    Class

--- main.cpp
+++ main.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 
-void rec(int start, int end) {
-    if ( start <= end ) {
-        if (start % 2 == 0) {
-            std::cout << start << ' ';
-            start += 2;
-            rec(start, end);
-        } else {
-            rec(++start, end);
-        }
+class animal {
+    int age;
+    std::string name;
+
+public:
+
+    static void print_hello() {
+        std::cout << "Hello" << "\n";
     }
-}
+
+    animal(int a, std::string n) {
+        this -> name = n;
+        this -> age = a;
+    }
+    ~animal(){
+        std::cout << "Name: " << this -> name << "\n" << "Age: " << this -> age << "\n";
+    }
+};
 
 int main() {
 
-    int num1, num2;
+    animal dog(12, "Bac");
 
-    std::cin >> num1 >> num2;
-
-    if(num1 < num2) {
-        rec(num1, num2);
-    } else {
-        rec(num2, num1);
-    }
+    dog.print_hello();
 
     return 0;
 }
