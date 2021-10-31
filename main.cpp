43f92ad3c581c65f3e6db52944fb73be780ca080
Andranik Grigoryan <andranik.grigoryan.02@gmail.com>
2021-10-31 21:19:14 +0400

    class_struct

--- main.cpp
+++ main.cpp
@@ -1,24 +1,41 @@
 #include <iostream>
 
-//
-//class Animal {
-//    int age;
-//    std::string name;
-//
-//public:
-//
-//    static void print_hello() {
-//        std::cout << "Hello" << "\n";
-//    }
-//
-//    Animal(int a, std::string n) {
-//        this -> name = n;
-//        this -> age = a;
-//    }
-//    ~Animal(){
-//        std::cout << "Name: " << this -> name << "\n" << "Age: " << this -> age << "\n";
-//    }
-//};
+class Animal {
+    int age;
+    std::string name;
+
+public:
+
+    static void print_hello() {
+        std::cout << "Hello" << "\n";
+    }
+
+    Animal(int a, std::string n) {
+        this -> name = n;
+        this -> age = a;
+    }
+    ~Animal(){                                                                              //destructor
+        std::cout << "Name: " << this -> name << "\n" << "Age: " << this -> age << "\n";
+    }
+};
+
+class User : public Animal{
+    std::string surname;
+
+protected:
+    void esim() {
+        std::cout << "Protected method" << "\n";
+    }
+
+public:
+    User(std::string s, std::string n, int a) : Animal(a, n) {
+        this -> surname = s;
+    }
+    ~User() {
+        std::cout << "Surname: " << this -> surname << "\n";
+        esim();
+    }
+};
 
 struct Human {
     std::string name;
@@ -34,17 +51,16 @@ void show_struct(Human* u) {
 
 int main() {
 
-//    Animal dog(12, "Bac");
+    User carl("Johnson", "Bac", 12);
+
+    Animal::print_hello();
+
+//    Human user;
+//    user.name = "John";
+//    user.height = 188;
+//    user.age = 21;
 //
-//    Animal::print_hello();
-
-    Human user;
-
-    user.name = "John";
-    user.height = 188;
-    user.age = 21;
-
-    show_struct(&user);
+//    show_struct(&user);
 
     return 0;
 }
