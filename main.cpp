8753d858feeb53b2fd14c54bc70810e3d09a3c23
Andranik Grigoryan <andranik.grigoryan.02@gmail.com>
2021-11-07 20:03:32 +0400

    find text

--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 #include "string"
 
+void fnd_text(std::string str, std::string f_str) {
+    int cnt = 0;
+    while (std::string::npos != str.find(f_str)) {
+        cnt++;
+        str.replace(str.find(f_str), f_str.size(), " ");
+    }
+    std::cout << cnt;
+}
+
 int main() {
 
-    std::string text = "Python is an interpreted high-level general-purpose programming language. Its design philosophy emphasizes code readability with its use of significant indentation. Its language constructs as well as its object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects";
+    std::string test = "Lorem Ipsum is simply dummy... Lorem Ipsum... Ipsum";
 
-    std::string test = "design";
-    int i = text.find(test);
+    std::string text = "Ipsum";
 
-    if(std::string::npos != i) {
-        std::cout << i << "  <<------>>  " << i - 1 + test.length();
-    } else {
-        std::cout << "ERROR: Word not found" << "\n";
-    }
-
+    fnd_text(test, text);
 
 
     return 0;
